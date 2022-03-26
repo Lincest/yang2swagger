93d05eef620be92f951c0bad3247b11c000b4a10
Lincest <474617032@qq.com>
2022-03-26 22:13:12 +0800

    feat: add generate.bat and add yang models.

---
+++ DATA/generate.bat
@@ -0,0 +1,8 @@
+@echo off
+set /p value=input module: 
+@echo on
+title yang swagger generator
+java -jar E:\code\projects\yang2swagger\cli\target\swagger-generator-cli-1.1.15-SNAPSHOT-executable.jar -yang-dir E:\code\projects\yang2swagger\DATA\yang -output E:\code\projects\yang2swagger\DATA\%value%-swagger.yaml %value%
+@echo ==================== generate %value%.swagger finished =====================
+pause
+exit
